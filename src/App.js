4c5af75e3d62ff98566ab292a6ace22d4533b1b2
unknown <mosampatel78@gmail.com>
2020-10-17 23:48:02 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [superheroData, setSuperheroData] = useState([]);
+
+  async function searchSuperHeroes () {
+    const response = await fetch(`https://www.superheroapi.com/api.php/10219177700206566/search/${searchText}`);
+    const data = await response.json();
+    console.log("searchSuperHeroes -> data", data)
+
+    setSuperheroData(data.results);
+  }
+
+  function handleChange (e) {
+    const searchTerm = e.target.value;
+    
+    setSearchText(searchTerm);
+    if (searchTerm.length === 0) {
+      setSuperheroData([]);
+    }
+    if (searchTerm.length > 3) {
+      searchSuperHeroes();
+    }
+  }
+
   return (
     <div className="App">
-     hello world
+      <Navbar />
+      <div className="main">
+        <SearchBar searchText={searchText} handleChange={handleChange} />
+        <SearchResults superheroData={superheroData} />
+      </div>
     </div>
   );
 }
